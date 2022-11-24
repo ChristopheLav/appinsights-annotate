391dc5c90735603ea9234ebcf44c14279ecb7d8d
Christophe Lavalle <christophe.lavalle@pinnula.fr>
2022-11-23 19:30:53 -0500

    Added some logs to debug

--- dist/index.js
+++ dist/index.js
@@ -55,6 +55,8 @@ const retryAttempt = 3;
     },
     // retry on error greater than 500
     retryCondition: error => {
+        console.log(error);
+        console.log(error.response);
         return !error.response || error.response.status >= 500;
     }
 });
@@ -102,6 +104,8 @@ function run() {
                     core.debug(`Locating Azure endpoint found: ` + endpoint);
                 })
                     .catch(err => {
+                    console.log(err);
+                    console.log(err.response);
                     if (err.response.status !== 200) {
                         throw new Error(`Failed to locate the Azure endpoint with status code: ${err.response.status} after ${retryAttempt} retry attempts`);
                     }
@@ -119,6 +123,8 @@ function run() {
                     }
                 })
                     .catch(err => {
+                    console.log(err);
+                    console.log(err.response);
                     if (err.response.status !== 200) {
                         throw new Error(`Failed to create the deployment annotation with status code: ${err.response.status} after ${retryAttempt} retry attempts`);
                     }
--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ axiosRetry(axios, {
 
   // retry on error greater than 500
   retryCondition: error => {
+    console.log(error);
+    console.log(error.response);
     return !error.response || error.response.status >= 500
   }
 })
@@ -66,6 +68,9 @@ async function run(): Promise<void> {
           core.debug(`Locating Azure endpoint found: ` + endpoint)
         })
         .catch(err => {
+          console.log(err);
+          console.log(err.response);
+          
           if (err.response.status !== 200) {
             throw new Error(
               `Failed to locate the Azure endpoint with status code: ${err.response.status} after ${retryAttempt} retry attempts`
@@ -90,6 +95,8 @@ async function run(): Promise<void> {
           }
         )
         .catch(err => {
+          console.log(err);
+          console.log(err.response);
           if (err.response.status !== 200) {
             throw new Error(
               `Failed to create the deployment annotation with status code: ${err.response.status} after ${retryAttempt} retry attempts`

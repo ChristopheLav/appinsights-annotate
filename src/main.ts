37c80f099e535b7d7e2f0ef86ac95a84a806261f
Christophe Lavalle <christophe.lavalle@pinnula.fr>
2023-11-23 07:18:30 -0500

    Fix code format

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import * as core from '@actions/core'
 
-import { AuthorizerFactory } from "azure-actions-webclient/AuthorizerFactory"
+import {AuthorizerFactory} from 'azure-actions-webclient/AuthorizerFactory'
 
-import { addAnnotation } from './utilities/AnnotationUtility'
+import {addAnnotation} from './utilities/AnnotationUtility'
 
 async function run(): Promise<void> {
   try {
-
     core.debug(`Reading action inputs`)
     const continueOnError = core.getBooleanInput('treat-error-as-warning')
     const applicationId = core.getInput('app-id')
@@ -14,18 +13,18 @@ async function run(): Promise<void> {
     const name = core.getInput('name')
 
     try {
-
-      core.debug(`Get the Azure authorization context`);
-      let endpoint;
+      core.debug(`Get the Azure authorization context`)
+      let endpoint
       try {
-        endpoint = await AuthorizerFactory.getAuthorizer();
+        endpoint = await AuthorizerFactory.getAuthorizer()
       } catch {
-        throw new Error("No credentials found. Please use the azure/login@v1 action before to attempt to create an Application Insights annotation.");
+        throw new Error(
+          'No credentials found. Please use the azure/login@v1 action before to attempt to create an Application Insights annotation.'
+        )
       }
 
       core.debug(`Create the Application Insights deployment annotation`)
       addAnnotation(endpoint, applicationId, name, isDeploymentSuccess)
-
     } catch (error) {
       if (continueOnError) {
         core.warning(`${(error as any)?.message ?? error}`)
@@ -33,7 +32,6 @@ async function run(): Promise<void> {
         throw error
       }
     }
-
   } catch (error) {
     core.setFailed(`${(error as any)?.message ?? error}`)
   }
